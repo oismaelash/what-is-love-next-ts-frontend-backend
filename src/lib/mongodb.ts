d76212d2711269d8170a607809d1f6596ab08d1c
oismaelash <contato@ismaelnascimento.com>
2025-05-01 10:57:07 -0300

    Restore MongoDB environment variable checks: Re-enable error handling for MONGODB_URI and MONGODB_DB to ensure necessary configurations are defined in the .env file.

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -8,13 +8,13 @@ declare global {
   };
 }
 
-// if (!process.env.MONGODB_URI) {
-//   throw new Error('Please define the MONGODB_URI environment variable inside .env');
-// }
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env');
+}
 
-// if (!process.env.MONGODB_DB) {
-//   throw new Error('Please define the MONGODB_DB environment variable inside .env');
-// }
+if (!process.env.MONGODB_DB) {
+  throw new Error('Please define the MONGODB_DB environment variable inside .env');
+}
 
 // Add your desired database name here
 const DB_NAME = process.env.MONGODB_DB; // Change this to your preferred database name
