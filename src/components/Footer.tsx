c55eab83820527c10df49f1c32845d7e7906212f
oismaelash <contato@ismaelnascimento.com>
2025-05-04 01:22:49 -0300

    Update Footer component styling and adjust highlight prices in constants: Remove fixed positioning from Footer for better layout flexibility, and revise pricing values in HIGHLIGHT_PRICES for accuracy.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
         px: 2,
         backgroundColor: (theme) => theme.palette.primary.main,
         color: 'white',
-        position: 'fixed',
+        // position: 'fixed',
         bottom: 0,
         left: 0,
         right: 0,
